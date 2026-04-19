9f05016d461e26fff16f5b6eebeb268faa613d08
Tanishq <tanishqrathore38@gmail.com>
2026-04-19 13:13:42 +0530

    Update date time.py

--- date time.py
+++ date time.py
@@ -3,6 +3,7 @@
 from datetime import datetime, timedelta
 curr_date = datetime.now()
 print('today '+ str(curr_date))
+
 # time delta is used to define a period of time
 week = timedelta(weeks=3)
 today= curr_date - week
