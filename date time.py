555e8ffea4f7f581364da35d18e2f958c186f10b
Tanishq <tanishqrathore38@gmail.com>
2026-04-23 13:55:29 +0530

    Update date time.py

--- date time.py
+++ date time.py
@@ -1,10 +1,8 @@
 # to use the current date and time to display
-# we use datetime library 
 from datetime import datetime, timedelta
-curr_date = datetime.now()
-print('today '+ str(curr_date))
+current_date = datetim.now()
+print(current_date)
 
-# time delta is used to define a period of time
-week = timedelta(weeks=3)
-today= curr_date - week
-print('tommorow is ' + str(today))
+today = timedelta(weeks =1)
+day = current_date - today 
+print(' the day one week before is ' + day)
